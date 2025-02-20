ae7c4273034627465973b18c5a053634f15c4734
Zaden Yip <hyfrancn@gmail.com>
2025-02-20 13:56:31 +0800

    finish exercise 5

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,14 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node* tortoise = head;
+    node* hare = head;
+    while (tortoise != NULL && hare != NULL && hare->next != NULL) {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
     return 0;
 }
